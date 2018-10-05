4768bfce242756c715f125772c381000da6e092f
GaussianBlurs <arnoldvbg@gmail.com>
2018-10-05 14:17:56 +0200

    Switch from template to Boostrap-Vue

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -1,11 +1,12 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import Argon from "./plugins/argon-kit";
+import Vue from 'vue'
+import BootstrapVue from "bootstrap-vue"
+import App from './App.vue'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap-vue/dist/bootstrap-vue.css"
 
-Vue.config.productionTip = false;
-Vue.use(Argon);
+Vue.use(BootstrapVue)
+
 new Vue({
-  router,
+  el: '#app',
   render: h => h(App)
-}).$mount("#app");
+})
