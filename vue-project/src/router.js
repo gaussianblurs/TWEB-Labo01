0b872265679388a30cc7880e071e7113e521b7d3
GaussianBlurs <arnoldvbg@gmail.com>
2018-10-05 11:47:54 +0200

    Setup

--- vue-project/src/router.js
+++ vue-project/src/router.js
@@ -2,50 +2,19 @@ import Vue from "vue";
 import Router from "vue-router";
 import AppHeader from "./layout/AppHeader";
 import AppFooter from "./layout/AppFooter";
-import Components from "./views/Components.vue";
-import Landing from "./views/Landing.vue";
-import Login from "./views/Login.vue";
-import Register from "./views/Register.vue";
-import Profile from "./views/Profile.vue";
+import Homepage from "./views/Homepage";
+import Profile from "./views/Profile";
 
 Vue.use(Router);
 
 export default new Router({
-  linkExactActiveClass: "active",
   routes: [
     {
       path: "/",
-      name: "components",
+      name: "homepage",
       components: {
         header: AppHeader,
-        default: Components,
-        footer: AppFooter
-      }
-    },
-    {
-      path: "/landing",
-      name: "landing",
-      components: {
-        header: AppHeader,
-        default: Landing,
-        footer: AppFooter
-      }
-    },
-    {
-      path: "/login",
-      name: "login",
-      components: {
-        header: AppHeader,
-        default: Login,
-        footer: AppFooter
-      }
-    },
-    {
-      path: "/register",
-      name: "register",
-      components: {
-        header: AppHeader,
-        default: Register,
+        default: Homepage,
         footer: AppFooter
       }
     },
@@ -58,12 +27,5 @@ export default new Router({
         footer: AppFooter
       }
     }
-  ],
-  scrollBehavior: to => {
-    if (to.hash) {
-      return { selector: to.hash };
-    } else {
-      return { x: 0, y: 0 };
-    }
-  }
+  ]
 });
---
+++ vue-project/src/views/Homepage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <hero></hero>
+  </div>
+</template>
+
+<script>
+import Hero from "./components/Hero";
+
+export default {
+  name: "homepage",
+  components: {
+    Hero
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
