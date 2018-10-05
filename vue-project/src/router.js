9bbfd3659a3bf1ba2fce31234a0e9fb7aa16c47e
GaussianBlurs <arnoldvbg@gmail.com>
2018-10-05 12:39:44 +0200

    Minor modifications

--- vue-project/src/router.js
+++ vue-project/src/router.js
@@ -27,5 +27,12 @@ export default new Router({
         footer: AppFooter
       }
     }
-  ]
+  ],
+  scrollBehavior: to => {
+    if (to.hash) {
+      return { selector: to.hash };
+    } else {
+      return { x: 0, y: 0 };
+    }
+  }
 });
--- vue-project/src/views/Homepage.vue
+++ vue-project/src/views/Homepage.vue
@@ -1,17 +1,12 @@
 <template>
   <div>
-    <hero></hero>
   </div>
 </template>
 
 <script>
-import Hero from "./components/Hero";
 
 export default {
-  name: "homepage",
-  components: {
-    Hero
-  }
+  name: "homepage"
 };
 </script>
 
