bfc61327d84da3c24234dd9b9ffb8578c0960ab4
psr <pierre.samuel.rochat@gmail.com>
2018-10-25 21:34:54 +0200

    build

--- vue-project/src/views/components/RepoForm.vue
+++ vue-project/src/views/components/RepoForm.vue
@@ -1,23 +1,35 @@
 <template>
   <div>
-    <div class="form-container">
-      <b-form-group
-      description="Enter a GitHub username to get sweet stats."
-      label="GitHub username"
-      label-for="input"
-      :invalid-feedback="invalidFeedback"
-      :valid-feedback="validFeedback"
-      :state="null"
-      >
-      <b-form-input id="input" :state="state" v-model.trim="username" placeholder="" ></b-form-input>
-    </b-form-group>
-  </div>
+    <div v-if="!this.code">
+      <div class="text-center">
+        <b-button class="login-button" variant="outline-dark" href="https://github.com/login/oauth/authorize?scope=user:email&client_id=404158819bf74ed09ba6">
+          <span class="logo-github">
+            <font-awesome-icon :icon="['fab', 'github']" />
+          </span>
+          <span class="text-login">Login with GitHub</span>
+        </b-button>
+        <p>Authenticate to get sweet stats about GitHub users.</p>
+      </div>
+    </div>
+    <div v-else>
+      <div class="form-container">
+        <b-form-group
+          description="Enter a GitHub username to get sweet stats."
+          label="GitHub username"
+          label-for="input"
+          :invalid-feedback="invalidFeedback"
+          :valid-feedback="validFeedback"
+          :state="null"
+        >
+          <b-form-input id="input" :state="state" v-model.trim="username" placeholder="" ></b-form-input>
+        </b-form-group>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import axios from '../../HTTP'
-
 export default {
   props: ['code'],
   methods: {
@@ -30,7 +42,6 @@ export default {
     }
   },
   mounted () {
-    console.log(`/authenticate/?code=${this.code}`)
     this.fetchToken()
     .then(() => console.log('done'))
   },
@@ -54,7 +65,7 @@ export default {
       username: ''
     }
   }
-}
+};
 </script>
 <style lang="scss" scoped>
 @import '../../assets/scss/repoform.scss';
