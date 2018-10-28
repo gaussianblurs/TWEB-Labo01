97d18a67908c04bffa0e84acacd9e17dd5a92b73
GaussianBlurs <arnoldvbg@gmail.com>
2018-10-29 00:51:52 +0100

    Unit test handles pagination

--- backend/test/test.js
+++ backend/test/test.js
@@ -1,8 +1,23 @@
 /* eslint-disable no-undef */
 const assert = require('assert')
+const Github = require('../src/Github')
+require('../node_modules/dotenv').config()
 
 describe('YOLO', () => {
   it('NO TIME', () => {
     assert.equal([1, 2, 3].indexOf(4), -1)
+  })
+})
+
+describe('Github', () => {
+  it('Handles pagination', () => {
+    const client = new Github()
+    client.repoCommits(process.env.OAUTH_TOKEN, 'gaussianblurs/TWEB-Project01', 2, 1)
+      .then(data => {
+        assert(data.length > 1)
+        done()
+      }, error => {
+        done(error)
+      })
   })
 })
